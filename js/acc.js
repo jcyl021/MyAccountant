ede7fa55404cf2415357877872fd25314bc2c04c
jcyl021 <liu_jasper@icloud.com>
2017-08-19 15:36:18 +0800

    succc

--- js/acc.js
+++ js/acc.js
@@ -13,42 +13,45 @@ function showmodal() {
 
 
 $("#spending").click(function() {
-  var occasion
-  if ($("#dining").prop("checked")) {
-    occasion = "dining"
-  } else if ($("#apparel").prop("checked")) {
-    occasion= "apparel"
-  } else if ($("#residential").prop("checked")) {
-    occasion= "residential"
-  } else if ($("#transportation").prop("checked")) {
-    occasion= "transportation"
-  } else if ($("#education").prop("checked")) {
-    occasion= "education"
-  } else if ($("#entertainment").prop("checked")) {
-    occasion= "entertainment"
-  } else {
-    occasion = " "
+  if ($("#date").val() != "" && $("#EvenT").val() != "" && $("#ammount").val() != "") {
+    var occasion
+    if ($("#dining").prop("checked")) {
+      occasion = "食"
+    } else if ($("#apparel").prop("checked")) {
+      occasion = "衣"
+    } else if ($("#residential").prop("checked")) {
+      occasion = "住"
+    } else if ($("#transportation").prop("checked")) {
+      occasion = "行"
+    } else if ($("#education").prop("checked")) {
+      occasion = "育"
+    } else if ($("#entertainment").prop("checked")) {
+      occasion = "樂"
+    } else {
+      occasion = " "
+    }
+    var newSpending = {
+      date: $("#date").val(),
+      EvenT: $("#EvenT").val(),
+      ammount: $("#ammount").val(),
+      spendingType: occasion
+    }
+    $("#date").val("");
+    $("#EvenT").val("");
+    $("#ammount").val("");
+    $("#dining").prop("checked", false);
+    $("#apparel").prop("checked", false);
+    $("#residential").prop("checked", false);
+    $("#transportation").prop("checked", false);
+    $("#education").prop("checked", false);
+    $("#entertainment").prop("checked", false);
+    accountingCollection.insert(newSpending);
+    accountingCollection.save();
+    alert("記帳成功 請OK")
+    // console.log(accountingCollection)
+  } else{
+    alert("必須輸入記帳內容")
   }
-  var newSpending = {
-    date: $("#date").val(),
-    EvenT: $("#EvenT").val(),
-    ammount: $("#ammount").val(),
-    spendingType: occasion
-  }
-  $("#date").val("");
-  $("#EvenT").val("");
-  $("#ammount").val("");
-  $("#dining").prop("checked", false);
-  $("#apparel").prop("checked", false);
-  $("#residential").prop("checked", false);
-  $("#transportation").prop("checked", false);
-  $("#education").prop("checked", false);
-  $("#entertainment").prop("checked", false);
-  accountingCollection.insert(newSpending);
-  accountingCollection.save();
-  alert("記帳成功 請OK")
-  // console.log(accountingCollection)
-
 })
 
 
---
+++ js/tenrecent.js
@@ -0,0 +1,34 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("accounting");
+var accountingCollection = db.collection('accounting');
+accountingCollection.load()
+
+
+function insertingString(color, date, EvenT, ammount, spendingType) {
+  return "<tr class="+color+"><td>" + date + "</td><td>" + EvenT + "</td><td>" + ammount + "</td><td>" + spendingType + "</td></tr>"
+}
+
+
+setTimeout(function() {
+  var accountings = accountingCollection.find({}, {
+    $orderBy: {
+      date: -1
+    },
+    $limit: 10
+  });
+  for (i = 0; i < accountings.length; i++) {
+    var colornumber=i%5;
+    if (colornumber == 0) {
+      var clr = "active"
+    } else if (colornumber == 1) {
+      var clr = "success"
+    } else if (colornumber == 2) {
+      var clr = "info"
+    } else if (colornumber == 3) {
+      var clr = "warning"
+    } else if (colornumber == 4) {
+      var clr = "danger"
+    }
+    $("#appendie").append(insertingString(clr, accountings[i].date, accountings[i].EvenT, accountings[i].ammount, accountings[i].spendingType))
+  }
+}, 1000)
